eb093ef59bdfcf593d0a522d20d5c58e84ee1b84
Timmy <mr.oshnokov@gmail.com>
2020-12-26 23:42:50 +0300

    hey

---
+++ OnlineShop/OnlineShop.js
@@ -0,0 +1,42 @@
+// let menu =  document.querySelector('#menu');
+// let box =  document.querySelector('#general');
+// let boxPosition = box.getBoundingClientRect().top;
+// window.addEventListener('scroll', function() {
+//     if (window.pageYOffset >= boxPosition / 50) {
+//         menu.style.top = '0px';
+//     } else {
+//         menu.style.top = '645px';
+//     }
+// });
+
+function openFirst() {
+    document.getElementById("firstSection").style.display = 'block';
+}
+
+function closeFirst() {
+    document.getElementById("firstSection").style.display = 'none';
+}
+
+function openSecond() {
+    document.getElementById("secondSection").style.display = 'block'
+}
+
+function closeSecond() {
+    document.getElementById("secondSection").style.display = 'none';
+}
+
+function openThird() {
+    document.getElementById("thirdSection").style.display = 'block'
+}
+
+function closeThird() {
+    document.getElementById("thirdSection").style.display = 'none';
+}
+
+function openFourth() {
+    document.getElementById("fourthSection").style.display = 'block'
+}
+
+function closeFourth() {
+    document.getElementById("fourthSection").style.display = 'none';
+}
---
+++ OnlineShop/README.md
@@ -0,0 +1,1 @@
+# OnlineShop-BMW-M5
